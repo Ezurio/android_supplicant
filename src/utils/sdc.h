d6731b553570463217ca3c5b6d2c5b3b0ed1aec6
Eric Bentley <eric.bentley@ezurio.com>
2024-09-03 14:39:56 -0400

    LRD-REL-A14-12.29.0.22

---
+++ src/utils/sdc.h
@@ -0,0 +1,27 @@
+/*
+ * wpa_supplicant - Summit extensions
+ * Copyright (c) 2010 -- Summit Data Communications
+ *
+ * This software may be distributed under the terms of BSD license.
+ *
+ * See README and COPYING for more details.
+ */
+
+#ifndef SDC_H
+#define SDC_H
+
+extern unsigned int sdc_eap_timeout;
+extern unsigned int laird_roam_delta;
+
+extern unsigned int laird_scan_dwell;
+extern unsigned int laird_scan_delay;
+extern unsigned int laird_scan_passive_dwell;
+extern unsigned int laird_scan_suspend_time;
+extern unsigned int laird_dfs_disable;
+#ifdef CONFIG_SDC_DMS
+extern unsigned int laird_dms;
+#endif
+
+extern int laird_read_config(void);
+
+#endif	// SDC_H
